18def857f3673357db3727eb51acf16873e592eb
omar paredes <petitenfant2014@gmail.com>
2023-12-15 10:51:42 +0100

    test api fetch6

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -11,7 +11,7 @@ export class ListComponent {
   private corsHeaders: HttpHeaders;
   public restaurantsArray$!: Observable<Irestaurant[]>;
   //urlIp: string = 'http://127.0.0.1:8080/easytable-0.0.1-SNAPSHOT';
-  urlIp: string = 'http://127.0.0.1:9000';
+  urlIp: string = 'http://localhost:9000';
   private urlApirestaurants = this.urlIp + '/restaurant/restaurants';
   constructor(private http: HttpClient) {
     this.corsHeaders = new HttpHeaders({
@@ -22,6 +22,7 @@ export class ListComponent {
   }
   ngOnInit(): void {
     this.restaurantsArray$ = this.getRestaurants();
+    headers: this.corsHeaders;
   }
   getRestaurants(): Observable<Irestaurant[]> {
     return this.http.get<Irestaurant[]>(this.urlApirestaurants);
