b5e0d97d643c1adf39354756e76e65e2c90b84ee
omar paredes <petitenfant2014@gmail.com>
2023-12-15 17:20:36 +0100

    success

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -4,18 +4,13 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor(private router:Router) {}
-
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
   onContinue(): void {
-    this.router.navigateByUrl('facesnaps');
-
+    this.router.navigateByUrl('list'); //facesnaps
   }
-
 }
