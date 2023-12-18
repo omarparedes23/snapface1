005dee0392428f88fed90021c47368a7aa7e13cd
omar paredes <petitenfant2014@gmail.com>
2023-12-18 22:56:39 +0100

    testing httpheader

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, catchError, tap } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http'; //HttpHeaders
 
 @Component({
   selector: 'app-list',
@@ -8,37 +8,46 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./list.component.scss'],
 })
 export class ListComponent {
-  private corsHeaders: HttpHeaders;
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': 'https://easytable.zapto.org',
+    }),
+  };
+  //private corsHeaders: HttpHeaders;
   public restaurantsArray$!: Observable<Irestaurant[]>;
-  //urlIp: string = 'http://127.0.0.1:8080/easytable-0.0.1-SNAPSHOT';
+  urlIp: string = 'http://127.0.0.1:8080/easytable-0.0.1-SNAPSHOT';
   //urlIp: string = 'http://52.47.152.220:9000';
   //urlIp: string = 'http://127.0.0.1:9000/api';
-  urlIp: string = '/api'; //descomentar
+  //urlIp: string = '/api'; //descomentar
   //urlIp: string = 'http://localhost:9000';
   private urlApirestaurants = this.urlIp + '/restaurant/restaurants';
   constructor(private http: HttpClient) {
-    this.corsHeaders = new HttpHeaders({
+    //
+    /*this.corsHeaders = new HttpHeaders({
       'Content-Type': 'application/json',
       Accept: 'application/json',
       'Access-Control-Allow-Origin': '*',
-    });
+    });*/
   }
   ngOnInit(): void {
     this.restaurantsArray$ = this.getRestaurants();
-    headers: this.corsHeaders;
+    //headers: this.corsHeaders;
   }
   getRestaurants(): Observable<Irestaurant[]> {
-    return this.http.get<Irestaurant[]>(this.urlApirestaurants).pipe(
-      tap((resultat) => console.log('Résultat de la requête : ', resultat)),
-      catchError((error) => {
-        if (error.error instanceof ErrorEvent) {
-          console.log(`Error: ${error.error.message}`);
-        } else {
-          console.log(`Error: ${error.message}`);
-        }
-        return [];
-      })
-    );
+    return this.http
+      .get<Irestaurant[]>(this.urlApirestaurants, this.httpOptions)
+      .pipe(
+        tap((resultat) => console.log('Résultat de la requête : ', resultat)),
+        catchError((error) => {
+          if (error.error instanceof ErrorEvent) {
+            console.log(`Error: ${error.error.message}`);
+          } else {
+            console.log(`Error: ${error.message}`);
+          }
+          return [];
+        })
+      );
   }
 }
 
